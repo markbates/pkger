14453cbd80232c5cef25c23a0a08777db1b10f15
Mark Bates <mark@markbates.com>
2019-09-21 18:00:53 -0400

    thats what i get

--- apply.go
+++ apply.go
@@ -1,24 +1,18 @@
 package pkger
 
 import (
-	"log"
 	"os"
-	"sync"
 
 	"github.com/markbates/pkger/pkging"
 	"github.com/markbates/pkger/pkging/pkgutil"
 )
 
 var current pkging.Pkger
-var gil = &sync.RWMutex{}
 
 func Apply(pkg pkging.Pkger, err error) error {
 	if err := pkgutil.Dump(os.Stdout, pkg); err != nil {
-		log.Fatal(err)
 		return err
 	}
-	gil.Lock()
-	defer gil.Unlock()
 	current = pkging.Wrap(current, pkg)
 	return nil
 }
