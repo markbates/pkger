050e38bcd2ef3942b5e2d4a7736f390d2803bb0a
Mark Bates <mark@markbates.com>
2019-09-22 12:29:35 -0400

    wild one

--- pkger_test.go
+++ pkger_test.go
@@ -42,3 +42,83 @@ func Test_AbsPath(t *testing.T) {
 	r.NoError(err)
 	r.Equal(filepath.Join(pwd, "rocket.ship"), s)
 }
+
+func Test_Current(t *testing.T) {
+	r := require.New(t)
+
+	info, err := Current()
+	r.NoError(err)
+	r.Equal("github.com/markbates/pkger", info.ImportPath)
+}
+
+func Test_Info(t *testing.T) {
+	r := require.New(t)
+
+	info, err := Info("github.com/markbates/pkger")
+	r.NoError(err)
+	r.Equal("github.com/markbates/pkger", info.ImportPath)
+}
+
+func Test_Create(t *testing.T) {
+	r := require.New(t)
+
+	MkdirAll("/tmp", 0755)
+	defer RemoveAll("/tmp")
+	f, err := Create("/tmp/test.create")
+	r.NoError(err)
+	r.Equal("/tmp/test.create", f.Name())
+	r.NoError(f.Close())
+}
+
+func Test_MkdirAll(t *testing.T) {
+	r := require.New(t)
+
+	_, err := Open("/tmp")
+	r.Error(err)
+	r.NoError(MkdirAll("/tmp", 0755))
+	defer RemoveAll("/tmp")
+
+	f, err := Open("/tmp")
+	r.NoError(err)
+	r.Equal("/tmp", f.Name())
+	r.NoError(f.Close())
+}
+
+func Test_Stat(t *testing.T) {
+	r := require.New(t)
+
+	info, err := Stat("/go.mod")
+	r.NoError(err)
+	r.Equal("/go.mod", info.Name())
+}
+
+func Test_Walk(t *testing.T) {
+	r := require.New(t)
+
+	files := map[string]os.FileInfo{}
+	err := Walk("/examples/app", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		files[path] = info
+		return nil
+	})
+	r.NoError(err)
+
+	r.Len(files, 20)
+}
+
+func Test_Remove(t *testing.T) {
+	r := require.New(t)
+
+	MkdirAll("/tmp", 0755)
+	defer RemoveAll("/tmp")
+	f, err := Create("/tmp/test.create")
+	r.NoError(err)
+	r.Equal("/tmp/test.create", f.Name())
+	r.NoError(f.Close())
+	r.NoError(Remove("/tmp/test.create"))
+
+	_, err = Stat("/tmp/test.create")
+	r.Error(err)
+}
