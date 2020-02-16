markbates/pkger

commit 4e1fe7b43f4b8575de9c86b101056ecb16a9e723

Add generated code disclaimer
Generated code should contain a disclaimer as per [1].

Add a disclaimer to the top of the file generated by the pack command.

Fixes #68.

[1] https://golang.org/s/generatedcode

cmd/pkger/cmds/pack.go
@@ -147,6 +147,7 @@ func Package(info here.Info, out string, decls parser.Decls) error {
 		name = "main"
 	}
 
+	fmt.Fprintf(f, "// Code generated by pkger; DO NOT EDIT.\n\n", name)
 	fmt.Fprintf(f, "package %s\n\n", name)
 	fmt.Fprintf(f, "import (\n\t\"github.com/markbates/pkger\"\n\t")
 	fmt.Fprintf(f, "\"github.com/markbates/pkger/pkging/mem\"\n)\n\n")

commit 9925b6cc05923fe499bd8cae27ccd2f2db6761b0

Fix typo
4e1fe7b43f4b8575de9c86b101056ecb16a9e723 introduced a Fprintf call that
passes a variable without a formatting directive. This will cause go vet
-printf to fail with

    cmd/pkger/cmds/pack.go:150: no formatting directive in Fprintf call

Drop the variable from the call.

cmd/pkger/cmds/pack.go
@@ -147,7 +147,7 @@ func Package(info here.Info, out string, decls parser.Decls) error {
 		name = "main"
 	}
 
-	fmt.Fprintf(f, "// Code generated by pkger; DO NOT EDIT.\n\n", name)
+	fmt.Fprintf(f, "// Code generated by pkger; DO NOT EDIT.\n\n")
 	fmt.Fprintf(f, "package %s\n\n", name)
 	fmt.Fprintf(f, "import (\n\t\"github.com/markbates/pkger\"\n\t")
 	fmt.Fprintf(f, "\"github.com/markbates/pkger/pkging/mem\"\n)\n\n")